10bed716e61a0f929b8177674e04418eaf888151
MavricMC <118589100+MavricMC@users.noreply.github.com>
2026-06-20 03:31:25 +1200

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,6 @@
+itemList = {
+    {"Bud light", "minecraft:potion", 5, false, "1"},
+    {"Cake", "minecraft:cake", 15, true, "1", 1, 15},
+    {"Cookie", "minecraft:cookie", 1, true, "5", 1, 64},
+    {"Milk", "minecraft:milk_bucket", 1, false, "2"}
+}
